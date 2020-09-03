38ebe0cd33f2a489581e7734de08cfcf6dc6c2c7
iPlanC <planc2333@outlook.com>
2020-09-03 16:19:36 +0800

    update gdb debugger

---
+++ 9.3/1.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-03 15:39:08
+ * @LastEditTime: 2020-09-03 16:18:49
+ * @FilePath: \Practical-Training-Neusoft-Tianjing\9.3\1.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    unsigned int i = 0;
+    unsigned int j = 0;
+    i++;
+    j = i;
+    printf("i = %d, j = %d", i, j);
+    return 0;
+}
