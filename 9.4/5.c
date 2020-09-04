6183dc87068cd844ba51e9c4e7bdbbf57096a36f
iPlanC <planc2333@outlook.com>
2020-09-04 16:50:39 +0800

    crete 9.4 5.c 6.c

---
+++ 9.4/5.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-04 16:03:13
+ * @LastEditTime: 2020-09-04 16:06:58
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.4\5.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("a = %d, b = %d, c = %d", a, b, c);
+    return 0;
+}
---
+++ 9.4/6.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-04 16:25:47
+ * @LastEditTime: 2020-09-04 16:26:38
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.4\6.c
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
