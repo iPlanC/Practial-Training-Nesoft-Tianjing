2f300e78ac76f12227bdfc022d2de8692f008e45
iPlanC <planc2333@outlook.com>
2020-09-04 15:34:46 +0800

    create 9.4 4.c

---
+++ 9.4/4.c
@@ -0,0 +1,22 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-04 15:23:16
+ * @LastEditTime: 2020-09-04 15:31:39
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.4\4.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    while (a < 20) {
+        a++;
+    }
+
+    do {
+        b++;
+    } while(b < 20);
+
+    return 0;
+}
