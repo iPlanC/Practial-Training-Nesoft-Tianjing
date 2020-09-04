63a45947810de6bd861a4a8ab3fae53994122cfe
iPlanC <planc2333@outlook.com>
2020-09-04 10:35:04 +0800

    update 9.4 1.c

--- 9.4/1.c
+++ 9.4/1.c
@@ -1,16 +1,18 @@
 /*
  * @Author: PlanC
  * @Date: 2020-09-04 08:57:37
- * @LastEditTime: 2020-09-04 10:15:17
+ * @LastEditTime: 2020-09-04 10:34:18
  * @FilePath: \Practical-Training-Neusoft-Tianjin\9.4\1.c
  */
 
 #include <stdio.h>
 
 int main() {
-    float edge1 = 3.0;
-    float edge2 = 4.0;
-    float edge3 = 5.0;
+    float edge1 = 0.0;
+    float edge2 = 0.0;
+    float edge3 = 0.0;
+    printf("input 3 edges length:\n");
+    scanf("%f %f %f", &edge1, &edge2, &edge3);
     printf("3 edge with %f %f %f, ", edge1, edge2, edge3);
     if ((edge1 + edge2 > edge3) && (edge1 + edge3 > edge2) && (edge2 + edge3 > edge1)) {
         printf("is triangle.\n");
