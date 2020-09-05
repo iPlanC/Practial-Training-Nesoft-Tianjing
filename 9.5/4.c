67eb25ee645dbfcc092e82dabfb6fb4c5656ed66
iPlanC <planc2333@outlook.com>
2020-09-05 14:39:04 +0800

    update 9.5 4.c

--- 9.5/4.c
+++ 9.5/4.c
@@ -1,7 +1,7 @@
 /*
  * @Author: PlanC
  * @Date: 2020-09-05 14:21:22
- * @LastEditTime: 2020-09-05 14:38:03
+ * @LastEditTime: 2020-09-05 14:38:54
  * @FilePath: \Practical-Training-Neusoft-Tianjin\9.5\4.c
  */
 
@@ -13,9 +13,9 @@ int main() {
     while (1) {
         printf("input a and b\n");
         scanf("%d %d", &a, &b);
-        printf("a = %d b = %d\n", a << 2, b >> 2);
+        printf("a = %d, b = %d\n", a << 2, b >> 2);
         printf("a | b = %d, a & b = %d\n", a | b, a & b);
-        printf("~a = %d ~b = %d\n", ~a, ~b);
+        printf("~a = %d, ~b = %d\n", ~a, ~b);
     }
     return 0;
 }
