ac1ee90d0aba13adfe120e0b9872bbade7d28390
iPlanC <planc2333@outlook.com>
2020-09-05 15:35:52 +0800

    create 9.5 5.c

---
+++ 9.5/5.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-05 15:30:34
+ * @LastEditTime: 2020-09-05 15:33:07
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.5\5.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    int array[10] = {0, 1, 2, 3, 4, 5, 6 ,7 ,8 , 9};
+    for (i = 0; i < 10; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+    return 0;
+}
