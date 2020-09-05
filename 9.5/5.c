3ad5d7f6a915c442ad1fce797709b78f6afa2587
iPlanC <planc2333@outlook.com>
2020-09-05 15:37:15 +0800

    update 9.5 5.c

--- 9.5/5.c
+++ 9.5/5.c
@@ -1,7 +1,7 @@
 /*
  * @Author: PlanC
  * @Date: 2020-09-05 15:30:34
- * @LastEditTime: 2020-09-05 15:33:07
+ * @LastEditTime: 2020-09-05 15:36:59
  * @FilePath: \Practical-Training-Neusoft-Tianjin\9.5\5.c
  */
 
@@ -9,7 +9,7 @@
 
 int main() {
     int i = 0;
-    int array[10] = {0, 1, 2, 3, 4, 5, 6 ,7 ,8 , 9};
+    int array[10] = {0, 1, 2, 3, 4, 5, 6 ,7 ,8 ,9};
     for (i = 0; i < 10; i++) {
         printf("array[%d] = %d\n", i, array[i]);
     }
