3b0cf89d5c66308e84cc82666012a3855fb01231
iPlanC <planc2333@outlook.com>
2020-09-05 09:16:43 +0800

    create 9.5 1.c

---
+++ 9.5/1.c
@@ -0,0 +1,25 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-05 09:02:38
+ * @LastEditTime: 2020-09-05 09:16:22
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.5\1.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    while (1) {
+        printf("input \"a\" and \"b\"\n");
+        scanf("%d %d", &a, &b);
+        printf("a = %d, b = %d, ", a, b);
+        if (a > 10 && b > 10) {
+            printf("all over 10.\n");
+        }
+        else {
+            printf("NOT all over 10.\n");
+        }
+    }
+    return 0;
+}
