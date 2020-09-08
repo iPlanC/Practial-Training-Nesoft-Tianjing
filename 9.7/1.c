6d37f9d68b2fc28e0a60843231cac1f0368266c5
iPlanC <planc2333@outlook.com>
2020-09-08 09:01:05 +0800

    init 9.7 & 9.8

---
+++ 9.7/1.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-07 09:15:03
+ * @LastEditTime: 2020-09-07 09:29:18
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.7\1.c
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
---
+++ 9.8/1.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: PlanC
+ * @Date: 2020-09-08 09:00:00
+ * @LastEditTime: 2020-09-08 09:00:30
+ * @FilePath: \Practical-Training-Neusoft-Tianjin\9.8\1.c
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
