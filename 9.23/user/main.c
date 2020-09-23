dc863eb16b40b7592a9f5deb1a1d872f0779eac7
PlanC <planc2333@outlook.com>
2020-09-23 14:12:40 +0800

    update

--- 9.23/user/main.c
+++ 9.23/user/main.c
@@ -6,50 +6,40 @@ void delay(unsigned int loop) {
 
 int main() {
 	int i = 0;
-	GPIO_InitTypeDef GPIO_A_init;
+	int flag = -1;
+	
+	GPIO_InitTypeDef GPIO_A_KEY_init;
+	GPIO_InitTypeDef GPIO_A_LED_init;
 	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); 
 	
-	GPIO_A_init.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
-	GPIO_A_init.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_A_init.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_A_KEY_init.GPIO_Pin = GPIO_Pin_0;
+	GPIO_A_KEY_init.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_A_KEY_init.GPIO_Mode = GPIO_Mode_IPU;
 	
-	GPIO_Init(GPIOA, &GPIO_A_init);
+	GPIO_A_LED_init.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
+	GPIO_A_LED_init.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_A_LED_init.GPIO_Mode = GPIO_Mode_Out_PP;
+	
+	GPIO_Init(GPIOA, &GPIO_A_KEY_init);
+	GPIO_Init(GPIOA, &GPIO_A_LED_init);
 	
 	while (1) {
-//		for (i = 1; i <= 4; i++) {
-//			GPIO_SetBits(GPIOA, (0x1 << i));
-//			delay(0xFFFFF);
-//			GPIO_ResetBits(GPIOA, (0x1 << i));
-//		}
-//		
-//		GPIO_SetBits(GPIOA, GPIO_Pin_1);
-//		delay(0xFFFFF);
-//		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-//		
-//		
-//		GPIO_SetBits(GPIOA, GPIO_Pin_2);
-//		delay(0xFFFFF);
-//		GPIO_ResetBits(GPIOA, GPIO_Pin_2);
-//		
-//		GPIO_SetBits(GPIOA, GPIO_Pin_3);
-//		delay(0xFFFFF);
-//		GPIO_ResetBits(GPIOA, GPIO_Pin_3);
-//		
-//		GPIO_SetBits(GPIOA, GPIO_Pin_4);
-//		delay(0xFFFFF);
-//		GPIO_ResetBits(GPIOA, GPIO_Pin_4);
-		for (i = 0; i < 5000; i++) {
-			GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_SET);
-			delay(0xFFF);
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0x0) {
+			if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0x0) {
+				flag = -flag;
+			}
 		}
-		GPIO_SetBits(GPIOA, GPIO_Pin_1);
-		delay(0xFFFFF);
-		for (i = 5000; i > 0; i--) {
-			GPIO_WriteBit(GPIOA, GPIO_Pin_1, Bit_RESET);
-			delay(0xFFF);
+		
+		if (flag == 1) {
+			for (i = 1; i <= 4; i++) {
+				GPIO_SetBits(GPIOA, (0x1 << i));
+			}
 		}
-		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-		delay(0xFFFFF);
+		else {
+			for (i = 1; i <= 4; i++) {
+				GPIO_ResetBits(GPIOA, (0x1 << i));
+			}
+		}
 	}
 }
